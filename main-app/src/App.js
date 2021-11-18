c2c099a19459fbf0fa6cbb2d33ba999ed6aab441
Maciej <m.nieweglowski@onet.pl>
2021-11-18 09:45:10 +0100

    v.0.00

---
+++ main-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NewTest from "./Components/NewTest";
+
+
+function App() {
+  return (
+    <NewTest />
+  );
+}
+
+export default App;
---
+++ main-app/src/Components/NewTest.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const TopContainer = styled.div `
+    text-align: right;
+`;
+
+function NewTest () {
+    return <TopContainer><div>Nowy tekst</div></TopContainer>
+}
+
+export default NewTest;
